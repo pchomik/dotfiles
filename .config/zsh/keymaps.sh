718c19a0c8e2b029f3ef97c61f473db8194073c3
Pawel Chomicki <pawel.chomicki@gmail.com>
2026-04-27 20:42:39 +0200

    feat: add more zsh keys

--- .config/zsh/keymaps.sh
+++ .config/zsh/keymaps.sh
@@ -5,10 +5,20 @@ function set_keymap() {
     bindkey "^[[1;5C" forward-word
     bindkey "^[[1;5D" backward-word
 
-    bindkey  "^[[H"   beginning-of-line
-    bindkey  "^[[F"   end-of-line
-    bindkey  "^[[3~"  delete-char
+    bindkey '^[[H'  beginning-of-line      # Home (Windows Terminal)
+    bindkey '^[[F'  end-of-line            # End (Windows Terminal)
+    bindkey '^[[1~' beginning-of-line      # Home (Alternative)
+    bindkey '^[[4~' end-of-line            # End (Alternative)
+    bindkey '^[[7~' beginning-of-line      # Home (Alternative)
+    bindkey '^[[8~' end-of-line            # End (Alternative)
+    bindkey '^OH'   beginning-of-line      # Home (Application mode)
+    bindkey '^OF'   end-of-line            # End (Application mode)
+
+    bindkey '^[[3~' delete-char            # Delete
+    bindkey '^[[3;5~' kill-word            # Ctrl+Delete (delete word)
+    bindkey '^?'    backward-delete-char    # Backspace
 
     export FZF_CTRL_T_OPTS="--preview 'bat --color=always --line-range :500 {}'"
     export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
+
 }
